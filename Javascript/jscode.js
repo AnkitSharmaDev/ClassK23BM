282248178a035480f6645e8284cf081f2261398b
Ankit <ankitsharma64604@gmail.com>
2025-03-25 11:19:02 +0530

    js tbc

---
+++ Javascript/jscode.js
@@ -0,0 +1,18 @@
+// var regno=12319809;
+// document.write("<h1 class='text-red-500'>this </h1>");
+
+// document.write('<h2 ">registration number is: '+ regno+"</h2>");
+// document.write('<h3 class="">Tailwind CSS </h3>');
+
+ 
+var a = 10, b = 20 , c = 30;
+document.write('<div class="text-center p-10 border-2 border-solid border-gray">');
+document.write((a>b) && (a>c)? "<h1 class='font-extrabold text-green-500'>true": "<h1 class='font-extrabold text-red-500'>false");
+document.write("<br>");
+document.write((b>a) || (b>c)? "<h1 class='font-extrabold text-green-500'>true": "<h1 class='font-extrabold text-red-500'>false");
+document.write("<br>");
+document.write(!(c>a) && (c>b)? "<h1 class='font-extrabold text-green-500'>true": "<h1 class='font-extrabold text-red-500'>false");
+
+
+
+
